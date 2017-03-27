d860a2f350bb5e3b6991f573339030a8a7eaf11b
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-27 16:36:41 +0100

    added shotgun

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello Steph"
 end
+
+get '/secret' do
+  "Hello Naz"
+end
+
+get '/rick' do
+  "Never gonna give you up"
+end
+
+get '/extension' do
+  "I always make sure to back up my data to the cloud"
+end
